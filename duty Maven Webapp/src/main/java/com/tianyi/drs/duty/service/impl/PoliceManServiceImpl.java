ccf45a4c6f5f56d846dc9f06905bf3e2cce05154
MrLee5212 <xiaohutuxian5212@163.com>
2014-10-09 16:47:36 +0800

    1647

--- duty Maven Webapp/src/main/java/com/tianyi/drs/duty/service/impl/PoliceManServiceImpl.java
+++ duty Maven Webapp/src/main/java/com/tianyi/drs/duty/service/impl/PoliceManServiceImpl.java
@@ -14,32 +14,26 @@ public class PoliceManServiceImpl implements PoliceManService{
     private PoliceManMapper policeDao;
  
 	public int deleteByPrimaryKey(Integer id) {
-		// TODO Auto-generated method stub
 		return policeDao.deleteByPrimaryKey(id);
 	}
  
 	public int insert(PoliceMan record) {
-		// TODO Auto-generated method stub
 		return policeDao.insert(record);
 	}
  
 	public int insertSelective(PoliceMan record) {
-		// TODO Auto-generated method stub
 		return policeDao.insertSelective(record);
 	}
  
 	public PoliceMan selectByPrimaryKey(Integer id) {
-		// TODO Auto-generated method stub
 		return policeDao.selectByPrimaryKey(id);
 	}
  
 	public int updateByPrimaryKeySelective(PoliceMan record) {
-		// TODO Auto-generated method stub
 		return policeDao.updateByPrimaryKey(record);
 	}
  
 	public int updateByPrimaryKey(PoliceMan record) {
-		// TODO Auto-generated method stub
 		return policeDao.updateByPrimaryKeySelective(record);
 	}
 }
