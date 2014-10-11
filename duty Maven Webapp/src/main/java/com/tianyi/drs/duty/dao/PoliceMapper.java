6724ea367a44001e5f42ea25e8affbc7231e1c63
MrLee5212 <xiaohutuxian5212@163.com>
2014-10-11 09:52:04 +0800

    0952

--- duty Maven Webapp/src/main/java/com/tianyi/drs/duty/dao/PoliceMapper.java
+++ duty Maven Webapp/src/main/java/com/tianyi/drs/duty/dao/PoliceMapper.java
@@ -1,7 +1,8 @@
 package com.tianyi.drs.duty.dao;
 
+import com.tianyi.drs.duty.dao.core.MyBatisRepository;
 import com.tianyi.drs.duty.model.Police;
-
+@MyBatisRepository
 public interface PoliceMapper {
     int deleteByPrimaryKey(Integer id);
 
