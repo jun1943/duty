8982322ca4e3ce2b71ed8f0d9d726d053231cad8
MrLee5212 <xiaohutuxian5212@163.com>
2014-10-14 16:31:06 +0800

    s

--- duty Maven Webapp/src/main/java/com/tianyi/drs/duty/service/WeaponService.java
+++ duty Maven Webapp/src/main/java/com/tianyi/drs/duty/service/WeaponService.java
@@ -1,5 +1,30 @@
 package com.tianyi.drs.duty.service;
 
+import java.util.List; 
+
+import com.tianyi.drs.duty.model.Weapon;
+ 
+
 public interface WeaponService {
 
+	int deleteByPrimaryKey(Integer id);
+
+    int insert(Weapon record);
+
+    int insertSelective(Weapon record);
+
+    Weapon selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Weapon record);
+
+    int updateByPrimaryKey(Weapon record);
+
+    Weapon findBycode(String code);
+    
+    Weapon findByname(String name);
+    
+     List<Weapon> selectAll();
+    
+     int updatePolice(Weapon weapon);
+   
 }
--- duty Maven Webapp/src/main/java/com/tianyi/drs/duty/service/impl/WeaponServiceImpl.java
+++ duty Maven Webapp/src/main/java/com/tianyi/drs/duty/service/impl/WeaponServiceImpl.java
@@ -1,10 +1,70 @@
 package com.tianyi.drs.duty.service.impl;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
+import com.tianyi.drs.duty.dao.PoliceMapper;
+import com.tianyi.drs.duty.dao.WeaponMapper;
+import com.tianyi.drs.duty.model.Weapon;
 import com.tianyi.drs.duty.service.WeaponService;
  
 @Service("weaponService")
 public class WeaponServiceImpl implements WeaponService {
+
+	@Resource(name="weaponMapper")
+	private WeaponMapper weaponMapper;
+	
+	public int deleteByPrimaryKey(Integer id) {
+		// TODO Auto-generated method stub
+		return weaponMapper.deleteByPrimaryKey(id);
+	}
+
+	public int insert(Weapon record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int insertSelective(Weapon record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Weapon selectByPrimaryKey(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int updateByPrimaryKeySelective(Weapon record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int updateByPrimaryKey(Weapon record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Weapon findBycode(String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Weapon findByname(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Weapon> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int updatePolice(Weapon weapon) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
