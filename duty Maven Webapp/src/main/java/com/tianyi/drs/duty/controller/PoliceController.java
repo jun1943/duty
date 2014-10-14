c73fb3b3bdf828052b476080938650496c35431f
MrLee5212 <xiaohutuxian5212@163.com>
2014-10-14 15:35:39 +0800

    s

--- duty Maven Webapp/src/main/java/com/tianyi/drs/duty/controller/PoliceController.java
+++ duty Maven Webapp/src/main/java/com/tianyi/drs/duty/controller/PoliceController.java
@@ -14,7 +14,7 @@ import com.tianyi.drs.duty.service.PoliceService;
 @Scope("prototype")
 @Controller
 @RequestMapping("/police")
-public class policecontroller {
+public class PoliceController {
 	@Resource(name = "policeService")
 	protected PoliceService policeService;// = ServiceFactory.getPoliceServiceInit();
 
